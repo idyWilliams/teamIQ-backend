69723537eca1ca59a2452e94356e95b20644dbe5
idyWilliams <widorenyin0@gmail.com>
2025-10-24 15:25:30 +0100

    fix imviatation table

--- app/models/invitation.py
+++ app/models/invitation.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.types import Enum as SQLEnum
+from sqlalchemy.sql import func
 from app.core.database import Base
 from app.models.organization import UserRole
-from sqlalchemy.types import Enum as SQLEnum
 import datetime
-from sqlalchemy.sql import func
+
 
 class Invitation(Base):
     __tablename__ = "invitations"
@@ -17,9 +18,13 @@ class Invitation(Base):
     )
     stack = Column(String, nullable=True)
     invitation_code = Column(String, unique=True, index=True, nullable=False)
-    expires_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow() + datetime.timedelta(days=1))
+    expires_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.datetime.utcnow() + datetime.timedelta(hours=48)
+    )
     accepted = Column(Boolean, default=False)
     organization_id = Column(Integer, ForeignKey("organizations.id"))
+    is_used = Column(Boolean, default=False)
 
     organization = relationship("Organization")
     createdAt = Column(DateTime(timezone=True), server_default=func.now())
